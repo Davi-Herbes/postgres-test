5abd8f7eaff9380f8301ed5ee2f886c7889f79d6
Davi Herbes <hmatosdavi@gmail.com>
2024-07-12 21:23:53 -0300

    Colocando configurações do codescapes

--- .devcontainer/init_postgres.sh
+++ .devcontainer/init_postgres.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+# Start the PostgreSQL service
 sudo service postgresql start
+
+# Update pg_hba.conf to allow password authentication
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/12/main/postgresql.conf
+echo "host all  all    0.0.0.0/0  md5" | sudo tee -a /etc/postgresql/12/main/pg_hba.conf
+
+# Restart PostgreSQL to apply changes
+sudo service postgresql restart
+
+# Set password for the postgres user
 sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres';"
+
+# Create a new database
 sudo -u postgres createdb mydatabase
